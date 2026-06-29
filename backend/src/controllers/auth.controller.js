b640461d936519c4844bb70f0baff8ea1fd5f639
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 00:24:52 +0100

    Refactor auth controller to use auth service methods

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,1 +1,11 @@
+const authService = require("../services/auth.service");
 
+exports.register = authService.register;
+
+exports.login = authService.login;
+
+exports.logout = authService.logout;
+
+exports.forgotPassword = authService.forgotPassword;
+
+exports.resetPassword = authService.resetPassword;
