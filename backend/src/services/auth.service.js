53a30ad8933ad67de26520c5b4a9da2abc1af9c2
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 00:26:46 +0100

    Add auth service with basic methods

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,21 @@
+const supabase = require("../config/supabase");
+
+exports.register = async (req, res) => {
+
+};
+
+exports.login = async (req, res) => {
+
+};
+
+exports.logout = async (req, res) => {
+
+};
+
+exports.forgotPassword = async (req, res) => {
+
+};
+
+exports.resetPassword = async (req, res) => {
+
+};
