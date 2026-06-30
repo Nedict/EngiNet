f23e10765871f2bf975f624d7cd9e33ceacba47c
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:47:50 +0100

    Add project routes for CRUD operations

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
 
+const projectController = require("../controllers/project.controller");
+
+router.get("/", projectController.getProjects);
+router.post("/", projectController.createProject);
+router.put("/:id", projectController.updateProject);
+router.delete("/:id", projectController.deleteProject);
+
+module.exports = router;
