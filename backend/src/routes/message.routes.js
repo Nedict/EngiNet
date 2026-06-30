bd61722d3193e4ff74ed7097a58eceeeaa3a1256
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:49:44 +0100

    Add message routes for getting and sending messages

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
 
+const messageController = require("../controllers/message.controller");
+
+router.get("/:conversationId", messageController.getMessages);
+router.post("/", messageController.sendMessage);
+
+module.exports = router;
