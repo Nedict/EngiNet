811f181d84d272dcfd52d747f337d8814f9785e2
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:04:21 +0100

    Refactor company service methods to empty stubs
    Removed implementation of company service methods and added empty method stubs.

--- backend/src/services/company.service.js
+++ backend/src/services/company.service.js
@@ -1,21 +1,5 @@
-const supabase = require("../config/supabase");
+exports.createCompany = async (req, res) => {};
 
-exports.createCompany = async (req, res) => {
+exports.getCompany = async (req, res) => {};
 
-};
-
-exports.getCompany = async (req, res) => {
-
-};
-
-exports.updateCompany = async (req, res) => {
-
-};
-
-exports.deleteCompany = async (req, res) => {
-
-};
-
-exports.listCompanies = async (req, res) => {
-
-};
+exports.updateCompany = async (req, res) => {};
