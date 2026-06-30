36c4b0ec0bc9c75e65c1dcbcf15da7c0304c3a69
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:05:04 +0100

    Refactor post service to empty function stubs
    Removed implementation details of post service functions.

--- backend/src/services/post.service.js
+++ backend/src/services/post.service.js
@@ -1,25 +1,7 @@
-const supabase = require("../config/supabase");
+exports.getPosts = async (req, res) => {};
 
-exports.createPost = async (req, res) => {
+exports.createPost = async (req, res) => {};
 
-};
+exports.updatePost = async (req, res) => {};
 
-exports.getPost = async (req, res) => {
-
-};
-
-exports.updatePost = async (req, res) => {
-
-};
-
-exports.deletePost = async (req, res) => {
-
-};
-
-exports.listPosts = async (req, res) => {
-
-};
-
-exports.likePost = async (req, res) => {
-
-};
+exports.deletePost = async (req, res) => {};
