b7cc9b4f6b5d49be7b40a06151d3710e8a4315d4
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:54:04 +0100

    Refactor project controller to use project service

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,1 +1,6 @@
+const projectService = require("../services/project.service");
 
+exports.getProjects = projectService.getProjects;
+exports.createProject = projectService.createProject;
+exports.updateProject = projectService.updateProject;
+exports.deleteProject = projectService.deleteProject;
