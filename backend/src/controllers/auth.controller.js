1ea6a56c4a99bcc61854f7a516dfbb05ca805835
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:47:57 +0100

    Fix resetPassword export position in auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,7 @@
 const authService = require("../services/auth.service");
 
 exports.register = authService.register;
-
 exports.login = authService.login;
-
 exports.logout = authService.logout;
-
 exports.forgotPassword = authService.forgotPassword;
-
 exports.resetPassword = authService.resetPassword;
