4904cc37dc1733850bf8c0ea1efccd77fedc0997
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:07:40 +0100

    Refactor community service methods to empty functions
    Removed existing community service methods and retained structure.

--- backend/src/services/community.service.js
+++ backend/src/services/community.service.js
@@ -1,25 +1,7 @@
-const supabase = require("../config/supabase");
+exports.getCommunities = async (req, res) => {};
 
-exports.createCommunity = async (req, res) => {
+exports.createCommunity = async (req, res) => {};
 
-};
+exports.updateCommunity = async (req, res) => {};
 
-exports.getCommunity = async (req, res) => {
-
-};
-
-exports.updateCommunity = async (req, res) => {
-
-};
-
-exports.deleteCommunity = async (req, res) => {
-
-};
-
-exports.listCommunities = async (req, res) => {
-
-};
-
-exports.joinCommunity = async (req, res) => {
-
-};
+exports.deleteCommunity = async (req, res) => {};
