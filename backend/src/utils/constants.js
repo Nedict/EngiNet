01c76369ab97acf59f3720434ee3b2e515415e81
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 08:09:02 +0100

    Add constants for account, post, job types, and status

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,27 @@
+exports.ACCOUNT_TYPES = {
+    PERSONAL: "personal",
+    COMPANY: "company"
+};
+
+exports.POST_TYPES = {
+    TEXT: "text",
+    IMAGE: "image",
+    VIDEO: "video",
+    DOCUMENT: "document",
+    PROJECT: "project"
+};
+
+exports.JOB_TYPES = {
+    FULL_TIME: "Full-Time",
+    PART_TIME: "Part-Time",
+    INTERNSHIP: "Internship",
+    CONTRACT: "Contract",
+    REMOTE: "Remote"
+};
+
+exports.APPLICATION_STATUS = {
+    PENDING: "Pending",
+    REVIEWED: "Reviewed",
+    ACCEPTED: "Accepted",
+    REJECTED: "Rejected"
+};
