62dd1ae1645d4694acced7cedbaf481796d7f98e
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:54:43 +0100

    Refactor job controller to use job service methods

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -1,1 +1,6 @@
+const jobService = require("../services/job.service");
 
+exports.getJobs = jobService.getJobs;
+exports.createJob = jobService.createJob;
+exports.updateJob = jobService.updateJob;
+exports.deleteJob = jobService.deleteJob;
