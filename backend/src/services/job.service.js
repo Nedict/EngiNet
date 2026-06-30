b22ff82271687c211ba15a720a1209bcd531dcae
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:06:54 +0100

    Refactor job.service.js to remove method bodies
    Removed implementations of job service methods.

--- backend/src/services/job.service.js
+++ backend/src/services/job.service.js
@@ -1,25 +1,7 @@
-const supabase = require("../config/supabase");
+exports.getJobs = async (req, res) => {};
 
-exports.createJob = async (req, res) => {
+exports.createJob = async (req, res) => {};
 
-};
+exports.updateJob = async (req, res) => {};
 
-exports.getJob = async (req, res) => {
-
-};
-
-exports.updateJob = async (req, res) => {
-
-};
-
-exports.deleteJob = async (req, res) => {
-
-};
-
-exports.listJobs = async (req, res) => {
-
-};
-
-exports.applyForJob = async (req, res) => {
-
-};
+exports.deleteJob = async (req, res) => {};
