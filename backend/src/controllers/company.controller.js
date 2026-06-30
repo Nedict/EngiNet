869c822efd3cd1520ff60f6815ef83bd34b0916c
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:52:45 +0100

    Refactor company controller to use service methods

--- backend/src/controllers/company.controller.js
+++ backend/src/controllers/company.controller.js
@@ -1,1 +1,5 @@
+const companyService = require("../services/company.service");
 
+exports.createCompany = companyService.createCompany;
+exports.getCompany = companyService.getCompany;
+exports.updateCompany = companyService.updateCompany;
