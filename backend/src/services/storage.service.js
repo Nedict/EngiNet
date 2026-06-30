105be17e079bc44adde155d435c55683d0319ca2
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 08:25:07 +0100

    Create storage service for file uploads

---
+++ backend/src/services/storage.service.js
@@ -0,0 +1,13 @@
+const supabase = require("../config/supabase");
+
+exports.uploadResume = async (file, userId) => {
+
+};
+
+exports.uploadProfilePhoto = async (file, userId) => {
+
+};
+
+exports.uploadCompanyLogo = async (file, companyId) => {
+
+};
