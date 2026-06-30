4b93ed07e6c731fe1e3b14491ffa65f127630906
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:52:06 +0100

    Refactor profile controller to use profile service

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -1,1 +1,6 @@
+const profileService = require("../services/profile.service");
 
+exports.getProfile = profileService.getProfile;
+exports.updateProfile = profileService.updateProfile;
+exports.uploadResume = profileService.uploadResume;
+exports.uploadPhoto = profileService.uploadPhoto;
