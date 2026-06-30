a079f2acb076061ea593cc0124d629bb199d1413
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:13:34 +0100

    Add JWT token generation and verification functions

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,1 +1,12 @@
+const jwt = require("jsonwebtoken");
+const env = require("../config/env");
 
+exports.generateToken = (payload) => {
+  return jwt.sign(payload, env.jwtSecret, {
+    expiresIn: "7d",
+  });
+};
+
+exports.verifyToken = (token) => {
+  return jwt.verify(token, env.jwtSecret);
+};
