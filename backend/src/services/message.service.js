92d0450edd85c5b0d83a0babb393793c3dcf5e8f
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:08:27 +0100

    Refactor message service methods
    Removed unused message service methods and added getMessages method.

--- backend/src/services/message.service.js
+++ backend/src/services/message.service.js
@@ -1,25 +1,3 @@
-const supabase = require("../config/supabase");
+exports.getMessages = async (req, res) => {};
 
-exports.sendMessage = async (req, res) => {
-
-};
-
-exports.getMessage = async (req, res) => {
-
-};
-
-exports.listMessages = async (req, res) => {
-
-};
-
-exports.deleteMessage = async (req, res) => {
-
-};
-
-exports.markAsRead = async (req, res) => {
-
-};
-
-exports.getConversations = async (req, res) => {
-
-};
+exports.sendMessage = async (req, res) => {};
