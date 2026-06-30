e3928191b2a7cbe652727457d0811eac1ab4f8ba
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 07:14:44 +0100

    Add getPagination function for pagination logic

--- backend/src/utils/pagination.js
+++ backend/src/utils/pagination.js
@@ -1,1 +1,14 @@
+exports.getPagination = (page = 1, limit = 10) => {
+  page = parseInt(page);
+  limit = parseInt(limit);
 
+  const from = (page - 1) * limit;
+  const to = from + limit - 1;
+
+  return {
+    page,
+    limit,
+    from,
+    to,
+  };
+};
