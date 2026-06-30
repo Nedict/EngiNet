63cfd2f1f76ad4efc4d53c4bc11326dd3371b270
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-30 06:55:22 +0100

    Refactor community controller to use service methods

--- backend/src/controllers/community.controller.js
+++ backend/src/controllers/community.controller.js
@@ -1,1 +1,6 @@
+const communityService = require("../services/community.service");
 
+exports.getCommunities = communityService.getCommunities;
+exports.createCommunity = communityService.createCommunity;
+exports.updateCommunity = communityService.updateCommunity;
+exports.deleteCommunity = communityService.deleteCommunity;
