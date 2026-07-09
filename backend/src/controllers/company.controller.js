7b52b5bbd26f6c238fd198a11bf30f11c0543f67
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-09 16:18:46 +0100

    Refactor company controller to use request handlers

--- backend/src/controllers/company.controller.js
+++ backend/src/controllers/company.controller.js
@@ -1,5 +1,22 @@
 const companyService = require("../services/company.service");
 
-exports.createCompany = companyService.createCompany;
-exports.getCompany = companyService.getCompany;
-exports.updateCompany = companyService.updateCompany;
+exports.createCompany = (req, res) =>
+    companyService.createCompany(req, res);
+
+exports.getMyCompany = (req, res) =>
+    companyService.getMyCompany(req, res);
+
+exports.getCompanyById = (req, res) =>
+    companyService.getCompanyById(req, res);
+
+exports.updateCompany = (req, res) =>
+    companyService.updateCompany(req, res);
+
+exports.deleteCompany = (req, res) =>
+    companyService.deleteCompany(req, res);
+
+exports.uploadLogo = (req, res) =>
+    companyService.uploadLogo(req, res);
+
+exports.uploadBanner = (req, res) =>
+    companyService.uploadBanner(req, res);
