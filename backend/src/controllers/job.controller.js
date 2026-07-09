f28fa357470a078ff9123b4fcb164f2d79efe67f
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-09 16:29:28 +0100

    Refactor job controller to use individual exports

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -1,6 +1,22 @@
 const jobService = require("../services/job.service");
 
-exports.getJobs = jobService.getJobs;
-exports.createJob = jobService.createJob;
-exports.updateJob = jobService.updateJob;
-exports.deleteJob = jobService.deleteJob;
+exports.createJob = (req, res) =>
+    jobService.createJob(req, res);
+
+exports.getAllJobs = (req, res) =>
+    jobService.getAllJobs(req, res);
+
+exports.getCompanyJobs = (req, res) =>
+    jobService.getCompanyJobs(req, res);
+
+exports.getJobsByDiscipline = (req, res) =>
+    jobService.getJobsByDiscipline(req, res);
+
+exports.getJob = (req, res) =>
+    jobService.getJob(req, res);
+
+exports.updateJob = (req, res) =>
+    jobService.updateJob(req, res);
+
+exports.deleteJob = (req, res) =>
+    jobService.deleteJob(req, res);
