ebf447cd3b73f667e414ca7829f6db45ceb9aa24
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-09 20:59:00 +0100

    Integrate authentication for post routes
    Added authentication middleware to post routes.

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,11 +1,45 @@
 const express = require("express");
+
 const router = express.Router();
+
+const authenticate = require("../middleware/auth.middleware");
 
 const postController = require("../controllers/post.controller");
 
-router.get("/", postController.getPosts);
-router.post("/", postController.createPost);
-router.put("/:id", postController.updatePost);
-router.delete("/:id", postController.deletePost);
+router.post(
+    "/",
+    authenticate,
+    postController.createPost
+);
+
+router.get(
+    "/feed",
+    authenticate,
+    postController.getFeed
+);
+
+router.get(
+    "/me",
+    authenticate,
+    postController.getMyPosts
+);
+
+router.get(
+    "/:id",
+    authenticate,
+    postController.getPost
+);
+
+router.put(
+    "/:id",
+    authenticate,
+    postController.updatePost
+);
+
+router.delete(
+    "/:id",
+    authenticate,
+    postController.deletePost
+);
 
 module.exports = router;
