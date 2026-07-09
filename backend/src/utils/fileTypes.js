a5769500dc299f448b8d11d542a9bb355f9dffd0
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-09 12:08:10 +0100

    Create fileTypes.js to specify supported file types
    Defines supported file types for images, documents, and videos.

---
+++ backend/src/utils/fileTypes.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    image: [
+
+        "image/jpeg",
+
+        "image/png",
+
+        "image/webp"
+
+    ],
+
+    document: [
+
+        "application/pdf",
+
+        "application/msword",
+
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+
+    ],
+
+    video: [
+
+        "video/mp4",
+
+        "video/webm",
+
+        "video/quicktime"
+
+    ]
+
+};
