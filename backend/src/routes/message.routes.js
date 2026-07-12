6f02133bb782c565c387cab88e64120c85541318
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-12 13:29:28 +0100

    Add authentication to message routes

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,9 +1,33 @@
 const express = require("express");
+
 const router = express.Router();
+
+const authenticate = require("../middleware/auth.middleware");
 
 const messageController = require("../controllers/message.controller");
 
-router.get("/:conversationId", messageController.getMessages);
-router.post("/", messageController.sendMessage);
+router.post(
+    "/:chatId",
+    authenticate,
+    messageController.sendMessage
+);
+
+router.get(
+    "/:chatId",
+    authenticate,
+    messageController.getMessages
+);
+
+router.put(
+    "/:messageId",
+    authenticate,
+    messageController.editMessage
+);
+
+router.delete(
+    "/:messageId",
+    authenticate,
+    messageController.deleteMessage
+);
 
 module.exports = router;
