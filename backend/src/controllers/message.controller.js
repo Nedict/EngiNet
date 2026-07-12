78592066e0fc935404f0984cef6cc326f130393a
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-07-12 13:31:19 +0100

    Refactor message controller to use explicit functions

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,13 @@
 const messageService = require("../services/message.service");
 
-exports.getMessages = messageService.getMessages;
-exports.sendMessage = messageService.sendMessage;
+exports.sendMessage = (req, res) =>
+    messageService.sendMessage(req, res);
+
+exports.getMessages = (req, res) =>
+    messageService.getMessages(req, res);
+
+exports.editMessage = (req, res) =>
+    messageService.editMessage(req, res);
+
+exports.deleteMessage = (req, res) =>
+    messageService.deleteMessage(req, res);
